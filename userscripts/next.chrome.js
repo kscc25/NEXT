0659bcf10c34c4287f2639d24fb98109d5e06067
Sang Hero <heroandtn3@gmail.com>
2016-01-30 22:39:53 +0700

    Add userscript for Chrome

---
+++ userscripts/next.chrome.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         NEXT
+// @description  The next generation of Agar.io extension
+// @version      0.0.0
+// @author       NEXT
+// @match        http://agar.io/*
+// @match        https://agar.io/*
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+window.stop(), document.documentElement.innerHTML = null, GM_xmlhttpRequest({
+  method: "GET",
+  url: "http://localhost:8080/agar.html",
+  onload: function(e) {
+    document.open(), document.write(e.responseText), document.close()
+  }
+});
