6c5e72e71b3c2214bf0f2b4d74d619912ed3c334
Acydwarp <acydwarp1@gmail.com>
2016-02-01 16:18:49 +0100

    JSPM, Gulp and Browser-sync
    Changed overall structure, added install instructions.

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp        = require('gulp');
+var cssMin      = require('gulp-cssnano');
+var htmlMin     = require('gulp-htmlmin');
+var rename      = require('gulp-rename');
+var jspm        = require('gulp-jspm');
+var runSeq      = require('run-sequence');
+var browserSync = require('browser-sync').create();
+
+gulp.task('css', function() {
+  gulp.src(['client/css/layout.css'])
+    .pipe(cssMin())
+    .pipe(rename({suffix: '.bundle'}))
+    .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('html', function() {
+  gulp.src(['client/index.html'])
+    .pipe(htmlMin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('js', function() {
+  gulp.src(['client/js/main.js'])
+    .pipe(jspm({selfExecutingBundle: true}))
+    .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('server', function() {
+  browserSync.init({
+    port: 8080,
+    notify: false,
+    open: false,
+    server: {
+      baseDir: "./build",
+      middleware: function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      }
+    }
+  });
+});
+
+gulp.task('build', function() {
+  runSeq(['css', 'html', 'js']);
+});
+
+gulp.task('watch', function () {
+  gulp.watch('client/css/**', ['css']);
+  gulp.watch('client/js/**', ['js']);
+  gulp.watch('client/index.html', ['html']);
+});
+
+gulp.task('launch', function() {
+  runSeq('build', 'server', 'watch');
+});
+
