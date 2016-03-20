456d74e8682b550412bfabce99689582cf9d1b6d
Albert Safin <xzfcpw@gmail.com>
2016-03-20 14:33:03 +0600

    NEXT-63 Correct map border

--- test/map-size.js
+++ test/map-size.js
@@ -20,6 +20,23 @@ describe('MapSize', function () {
     expect(new MapSize(-10, -20, 30, 50).height()).to.be.equal(70);
   });
 
+  describe('.isLegit()', function () {
+    it('should be true on full world size', function () {
+      expect(new MapSize(-6796.050310292858,
+                         -9203.937550139428,
+                         +7346.085313438094,
+                         +4938.198073591524).isLegit())
+            .to.be.true;
+    });
+    it('should be false on partial world size', function () {
+      expect(new MapSize(-3517.222111139978,
+                         +1457.9073725840553,
+                         +2242.777888860022,
+                         +4697.907372584055).isLegit())
+            .to.be.false;
+    });
+  });
+
   it('should have default() constructor', function () {
     expect(MapSize.default()).to.be.an.instanceof(MapSize);
   });
